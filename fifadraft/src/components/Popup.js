a978f1d3d25a3a6e1abd037d4fe74ecf9d324a22
Nathan Monette <nathanmonette1@gmail.com>
2023-03-30 17:15:22 -0700

    Successfully Added Registration

--- fifadraft/src/components/Popup.js
+++ fifadraft/src/components/Popup.js
@@ -28,17 +28,20 @@ class User {
 export default function CustomPopup({ users, updateUsers, menus, updateMenus }) {
     const value = useRef('')
     const [roster, updateRoster] = useState([])
+    const ref = useRef()
     return (
     <>
-    <Popup trigger={<Button variant="contained" startIcon={<Avatar src={'https://www.shareicon.net/data/128x128/2015/12/01/680848_vertical_512x512.png'} />}></Button>} position="center center" modal>
+    <Popup ref={ref} trigger={<Button variant="contained" startIcon={<Avatar src={'https://www.shareicon.net/data/128x128/2015/12/01/680848_vertical_512x512.png'} />}></Button>} position="center center" modal>
         <center>
-            <div><h2>Enter Username</h2></div>
+            <div><h2>Registration</h2></div>
             <div><TextField id="outlined-basic" label="Username" variant="outlined" inputRef={value} required></TextField></div>
             <div><Button variant="contained" onClick={() => {
+              if (value.current.value.length != 0){
                 const user = new User({update: updateRoster, roster: roster, title: value.current.value})
                 updateUsers([...users, user])
                 updateMenus([...menus, user.comp])
-            }} disableElevation>Register</Button></div>
+                ref.current.close()
+            }}} disableElevation>Register</Button></div>
         </center>
     </Popup>
     </>
